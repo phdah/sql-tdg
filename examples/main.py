56cea15a0e31339b414135df2c592c82ad114b74
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-03-17 15:38:14 +0100

    Add example code

---
+++ examples/main.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+import duckdb
+from sql_tdg.tdg.data import TDG
+from sql_tdg.tdp.parser import Parser
+from sql_tdg.tdg.types import (
+    ColType,
+    Schema,
+)
+
+
+table_name = "tdg_table"
+query = f"""
+    select
+        a, b, c, d
+    from {table_name}
+         where a > 100
+            and a <= 900
+            and (a >= 110 or a < 1000) -- Try Paren type
+            and b = 'hello' -- Ensures not distinct
+            and b != 'no hello' -- Try not equal to
+"""
+
+
+schema = Schema(
+    cols=[
+        ColType("a", int),
+        ColType("b", str),
+        ColType("c", datetime),
+        ColType("d", bool),
+    ],
+)
+
+if __name__ == "__main__":
+    # Parse query
+    p = Parser(query)
+    p.parseQuery()
+
+    # Construct Test Data Generator object (TDG)
+    data = TDG(schema=schema, conditions=p.conditions)
+
+    # Setup a duckdb connection
+    conn = duckdb.connect()
+    # Get data, and convert to duckdb table
+    _ = data.getData().to_duckdb(conn)
+
+    # Insert "bad" data
+    conn.sql("insert into tdg_table values (1, 'mark', '1970-01-01 01:00:19', False)")
+    conn.sql(
+        "insert into tdg_table values (120, 'no hello', '1970-01-01 01:00:20', False)"
+    )
+
+    # Show all data, and filtered data
+    conn.table(table_name).show()
+    conn.sql(query).show()
